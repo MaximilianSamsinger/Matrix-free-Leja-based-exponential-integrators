f2a236739c31cf1bd935d84d49dbabad0a808be9
Maximilian Samsinger <m.samsinger@gmail.com>
2019-02-12 16:29:49 +0100

    Updating the unit tests for LinearOperators

--- AdvectionDiffusion1D.py
+++ AdvectionDiffusion1D.py
@@ -1,7 +1,9 @@
 import numpy as np
 from scipy.sparse import dia_matrix
+from scipy.sparse.linalg import LinearOperator
 
-def AdvectionDiffusion1D(N, adv_coeff, dif_coeff, periodic = True , h = None):
+def AdvectionDiffusion1D(N, adv_coeff, dif_coeff, periodic = True , h = None, 
+                         asLinearOp = False):
     '''
     Input:
         N... Number of rows and columns of Matrix A
@@ -11,9 +13,10 @@ def AdvectionDiffusion1D(N, adv_coeff, dif_coeff, periodic = True , h = None):
                     otherwise we assume homogeneous Dirichlet boundary cond.
         h... float, space discretization length. If None, we discretize on the
              interval of unit length (for example [0,1])
+        asLinearOp... If True, return a LinearOperator instead of a Matrix
             
     Output:
-        A... NxN sparse Advection-Diffusion matrix
+        A... NxN sparse matrix or LinearOperator  
         u... Nx1 numpy array for testing
         
     
@@ -29,29 +32,64 @@ def AdvectionDiffusion1D(N, adv_coeff, dif_coeff, periodic = True , h = None):
     if h is None:
         h = 1./(N-1) #Assume space dimension is interval of unit length
     
+    if asLinearOp:
+        ''' Create linear Operator '''
+        if periodic:
+            def mv(v):
+                # Advection (Upwind)
+                adv = -v
+                adv[:-1] += v[1:]
+                adv[-1] += v[0]
+                
+                # Diffusion
+                dif = -2*v
+                dif[:-1] += v[1:]
+                dif[1:] += v[:-1]
+                dif[-1] += v[0]
+                dif[0] += v[-1]
+                
+                return adv_coeff/h * adv + dif_coeff/h**2 * dif
+        else:
+            def mv(v):
+                # Advection (Upwind)
+                adv = -v
+                adv[:-1] += v[1:]
+                
+                # Diffusion
+                dif = -2*v
+                dif[:-1] += v[1:]
+                dif[1:] += v[:-1]
+                
+                return adv_coeff/h * adv + dif_coeff/h**2 * dif
+        
+        A = LinearOperator((N,N), matvec = mv)
     
-    if periodic:
-        adv_data = np.array([[-1],[1],[1]]).repeat(N, axis=1)/h
-        adv_offsets = np.array([0, 1, -(N-1)])
-        
-        dif_data = np.array([[-2],[1],[1],[1],[1]]).repeat(N, axis=1)/h**2
-        dif_offsets = np.array([0, -1, 1, -(N-1), N-1])
-        
     else:
-        adv_data = np.array([[-1],[1]]).repeat(N, axis=1)/h #Upwind
-        adv_offsets = np.array([0, 1])
+        ''' Create sparse Matrix '''
+        if periodic:
+            adv_data = np.array([[-1],[1],[1]]).repeat(N, axis=1) #Upwind
+            adv_offsets = np.array([0, 1, -(N-1)])
+            
+            dif_data = np.array([[-2],[1],[1],[1],[1]]).repeat(N, axis=1)
+            dif_offsets = np.array([0, -1, 1, -(N-1), N-1])
+            
+        else:
+            adv_data = np.array([[-1],[1]]).repeat(N, axis=1) #Upwind
+            adv_offsets = np.array([0, 1])
+            
+            dif_data = np.array([[-2],[1],[1]]).repeat(N, axis=1)
+            dif_offsets = np.array([0, -1, 1])
         
-        dif_data = np.array([[-2],[1],[1]]).repeat(N, axis=1)/h**2
-        dif_offsets = np.array([0, -1, 1])
-        
-        
-    #Advection
-    A = adv_coeff * dia_matrix((adv_data, adv_offsets), shape=(N, N)) 
-    #Diffusion
-    A += dif_coeff * dia_matrix((dif_data, dif_offsets), shape=(N, N)) 
+        #Advection
+        A = adv_coeff/h * dia_matrix((adv_data, adv_offsets), shape=(N, N)) 
+        #Diffusion
+        A += dif_coeff/h**2 * dia_matrix((dif_data, dif_offsets), shape=(N, N)) 
     
     x = np.linspace(0,1,N+2)[1:N+1]
     u = np.exp(-80*((x-0.55)**2))
     u = u.reshape((N, 1)) #appropriate initial vector 
     
     return A, u
+    
+
+    
--- unit_tests.py
+++ unit_tests.py
@@ -3,17 +3,20 @@ import scipy as sp
 from AdvectionDiffusion1D import AdvectionDiffusion1D
 from expleja import expleja
 
-N = 100
-adv_coeff = 2.
-dif_coeff = 0.99
-periodic = True
-h = None
+N = 200
+adv_coeff = 1
+dif_coeff = 1
+periodic = False
+h = 1
+asLinearOp = True
+
+A, u = AdvectionDiffusion1D(N, adv_coeff, dif_coeff, periodic, h, asLinearOp)
+
 
 '''
 Test if AdvectionDiffusion Matrix delivers plausible results
 '''
-A, u = AdvectionDiffusion1D(N, adv_coeff, dif_coeff, periodic, h)
-print(A.toarray())
+#print(A.toarray())
 
 '''
 Test if the periodic and non-periodic case have the same result, except for 
@@ -25,14 +28,42 @@ Truth_vector = np.isclose(A1 @ u,A2 @ u)[1:-1]
 assert(np.all(Truth_vector))
 
 '''
-Expleja: Is the result plausible
+expleja: Is the result plausible?
 '''
 
 tau = 1.
 
-expAv1 = sp.sparse.linalg.expm_multiply(tau*A, u)
+A_sparse, u = AdvectionDiffusion1D(N, adv_coeff, dif_coeff, periodic, h, False)
+expAv1 = sp.sparse.linalg.expm_multiply(tau*A_sparse, u)
 expAv2 = expleja(tau, A, u)[0]
 Truth_vector = np.isclose(expAv1, expAv2)
 assert(np.all(Truth_vector))
 
+'''
+expleja: Test with zero vector
+'''
+
+A1 = AdvectionDiffusion1D(N, adv_coeff, dif_coeff, periodic, h, False)[0] # Sparse Matrix
+A2 = A1.toarray() # 2D Numpy Array
+A3 = AdvectionDiffusion1D(N, adv_coeff, dif_coeff, periodic, h, True)[0] # Linear Operator 
+
+tau = 1.
+
+def test_with_0_vector(tau, matrix):
+    v = np.zeros((N,1))
+    assert(expleja(tau, matrix, v)[0] is v)
+    v = v.flatten()
+    assert(expleja(tau, matrix, v)[0] is v)
+
+test_with_0_vector(tau, A1)
+test_with_0_vector(tau, A2)
+test_with_0_vector(tau, A3)
+
 print('Everything is ok')
+
+'''
+%timeit A@u
+%timeit A_sparse@u
+%memit A@u
+%memit
+'''
